78c57768a1470b3f8b8a7f6a0e7a022d0685873a
BrydeRov <andresjavieroksi@gmail.com>
2022-11-20 13:28:08 -0600

    Componente Videos y API con Axios

--- src/Components/Feed.jsx
+++ src/Components/Feed.jsx
@@ -1,7 +1,18 @@
 import {useState, useEffect } from 'react';
 import { Box, Stack, Typography } from '@mui/material'; 
-import SideBar from './SideBar';
+import {SideBar, Videos } from './'
+import { fetchFromAPI } from '../utils/fetchFromApi'
+
 function Feed() {
+
+  const [selectedCategory, setSelectedCategory] = useState('New');
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
+    .then((data) => setVideos(data.items))
+  }, [selectedCategory]);
+
   return (
     <Stack
       sx={{
@@ -24,7 +35,10 @@ function Feed() {
           }
         }}
       >
-        <SideBar />
+        <SideBar 
+          selectedCategory={selectedCategory}
+          setSelectedCategory={setSelectedCategory}
+        />
         <Typography className="copyright" variant="body2"
           sx={{
             mt: 1.5,
@@ -34,6 +48,22 @@ function Feed() {
           Copyright 2022 JS Media
         </Typography>
       </Box>
+
+      <Box p={2} sx={{overflowY: 'auto', height: '90vh', flex: 2}}>
+        <Typography variant="h4" fontWeight="bold" mb={2} color="white">
+            {selectedCategory}
+            <span
+              style={{
+                color: '#F31503',
+                paddingLeft: 10
+              }}
+            >
+              Videos
+            </span>
+        </Typography>
+      </Box>
+
+      <Videos videos={videos}/>
     </Stack>
   )
 }
